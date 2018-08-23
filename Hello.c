476a588fe930ffba5267e7e596ab70afe639c29d
Fahmyazhar <fahmyshing@gmail.com>
2018-08-23 10:34:46 -0500

    Hello.c

---
+++ Hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Muhamad Azhar
+ * Date: 2018/08/23
+ *
+ * A simple hello program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+	printf("Hello Muhamad Azhar Mechanical Engineering!\n");
+	
+	return 0;
+}
